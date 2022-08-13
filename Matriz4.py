e265442e1129d14e0a3c3b2ac189ce144f43415e
Alejandro Torrez <ciberthingscomputinglearning@outlook.com>
2022-08-13 16:08:30 -0600

    fix linting

--- Matriz4.py
+++ Matriz4.py
@@ -2,16 +2,16 @@
 M = []
 longitud = 0
 dimension = 0
- 
+
 # Pedimos la dimensión de la matriz,
 dimension = int(input("Dimension de la matriz: "))
- 
+
 # Creamos una matriz nula...
 Matriz = []
- 
+
 for elemento in range(dimension):
-    Matriz.append ([0] * dimension)
- 
+    Matriz.append([0] * dimension)
+
 # y leemos su contenido
 for fila in range(dimension):
     for columna in range(dimension):
@@ -19,10 +19,10 @@ for fila in range(dimension):
         if fila == columna:
             # Guarda el número 1 en la posición
             Matriz[fila][columna] = 1
-print("\n>>> MATRIZ M(%dx%d): %s\n" %(dimension,dimension,M))
- 
+print("\n>>> MATRIZ M(%dx%d): %s\n" % (dimension, dimension, M))
+
 longitud = len(M)
- 
+
 # Se imprime cada fila de la matriz
 for valor in range(longitud):
     print(M[valor])
