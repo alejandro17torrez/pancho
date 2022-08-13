3be4ba95e062dfab5279da71c49eb81e9b64db51
Francisco Antonio Espinoza Chavarría <dametufuerzapegazo71@gmail.com>
2022-08-13 16:45:52 -0600

    ecuacion de primer grado (incompleta)

---
+++ ecuacion_primer_grado.py
@@ -0,0 +1,16 @@
+# Declaracion de Variables
+a = 0 
+b = 0
+x = 0 
+
+a = float(input("a: "))
+b = float(input("b: "))
+
+
+x = -b/a
+print(">>> SOLUCION: x = ", x)
+
+if b != 0:
+	print:("LA ECUACION NO TIENE SOLUCION")
+else:
+	print("LA ECUACION TIENE INFINITAS SOLUCIONES")
